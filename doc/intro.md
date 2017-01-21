187efb7d0c81d9dd128b72f1226ecfe67295985e
Thomas Hallgren <thomas@tada.se>
2017-01-21 17:10:07 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pcore-examples
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/pcore_examples/core.clj
@@ -0,0 +1,40 @@
+(ns pcore-examples.core
+  (:gen-class)
+  (:import [com.puppet.pcore Pcore]
+           [com.puppet.pcore.serialization SerializationFactory]
+           [java.util.function Supplier]
+           [java.io BufferedInputStream]
+           [java.io FileInputStream]
+           [com.fasterxml.jackson.databind ObjectMapper]
+           (java.util List HashMap)))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(def mapper (ObjectMapper.))
+
+(defn declare-type [name type-string]
+  (.declareType (Pcore/typeEvaluator) name type-string))
+
+(defn resolve-type [type-string]
+  (.resolve (Pcore/typeEvaluator) type-string))
+
+(defn with-local-scope
+  [func & args]
+  (Pcore/withLocalScope (reify Supplier (get [this] (apply func args)))))
+
+(defn chunk-deserializer
+  []
+  (.forInputChunks (Pcore/serializationFactory SerializationFactory/JSON)))
+
+(defn buffered-input
+  [path]
+  (BufferedInputStream. (FileInputStream. path)))
+
+(defn parse-json
+  [path]
+  (.readValue mapper (buffered-input path) HashMap))
+
+(defn strings-to)
